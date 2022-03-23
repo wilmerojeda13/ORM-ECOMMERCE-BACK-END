29d26448130728e62e08e99613d23091b20be5e5
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-03-23 12:10:05 -0400

    adding a models folder and sourcing in the index.js products by category and the other way around

---
+++ models/index.js
@@ -0,0 +1,36 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+const { type } = require('os');
+const { DataTypes } = require('sequelize/types');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey:'category_id',
+})
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+})
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
