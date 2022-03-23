4d526a26fac8e89fc4c737e6045a5534768b7f29
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-03-23 12:36:04 -0400

    defining the id columns for product

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,14 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define id columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    }
+
   },
   {
     sequelize,
