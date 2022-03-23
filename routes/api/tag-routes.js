c395e941ffa3a794755ed8476579296b134b220d
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-03-23 19:57:38 -0400

    add adding a single tag by its id using its method and function

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,6 +24,18 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
 });
 
 router.post('/', (req, res) => {
